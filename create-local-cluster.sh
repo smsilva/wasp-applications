00af2ba55ce2035b8ff2b4a094aa7d720dd41c4e
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-06 12:03:39 -0300

    refactor(scripts): Rename values and scripts

--- create-local-cluster.sh
+++ create-local-cluster.sh
@@ -1,8 +1,14 @@
 #/bin/bash
+
+# Create a Local Kind Cluster 
 kustomize/kind/create-cluster.sh
+
+# Install ArgoCD
 kustomize/argocd/install.sh
 
+# Create an ArgoCD Application
 ./create-argocd-apps-of-apps.sh \
-  infrastructure/bootstrap/app-of-apps/values-wasp-dev-na-north-1.yaml
+  infrastructure/bootstrap/app-of-apps/values-wasp-dev-na-north.yaml
 
+# Retrieve ArgoCD Information
 ./scripts/argocd_retrieve_initial_admin_password.sh
