c3d845ce9f03cc42eb22c346e5c4b50bdda86a86
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-06 20:16:20 -0300

    refactor(infrastructure/bootstrap/app-of-apps/): Rename files and update values

--- create-local-cluster.sh
+++ create-local-cluster.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+REGION_NAME="wasp-dev-na-eastus2"
+
 # Create a Local Kind Cluster 
 kustomize/kind/create-cluster.sh
 
@@ -8,7 +10,7 @@ kustomize/argocd/install.sh
 
 # Create an ArgoCD Application
 ./create-argocd-apps-of-apps.sh \
-  infrastructure/bootstrap/app-of-apps/values-wasp-dev-na-north.yaml
+  "infrastructure/bootstrap/app-of-apps/values-${REGION_NAME?}.yaml"
 
 # Retrieve ArgoCD Information
 ./scripts/argocd_retrieve_initial_admin_password.sh
