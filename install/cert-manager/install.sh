1cbdba4b5989a29f047ee2e730d5da73b2733df6
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-13 19:10:42 -0300

    feat(install/cert-manager/install.sh): Install cert-manager

---
+++ install/cert-manager/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo ""
+
+SCRIPT_PATH="$(dirname $0)"
+
+helm repo add jetstack https://charts.jetstack.io
+
+helm repo update jetstack
+
+helm search repo jetstack
+
+helm upgrade \
+  --install \
+  --namespace cert-manager \
+  --create-namespace \
+  --set installCRDs=true \
+  cert-manager jetstack/cert-manager
