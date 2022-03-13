51e88e5ec5b5f3663a7ced0b632d5b6a955f2bde
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-13 12:18:47 -0300

    feat(install/argocd/install.sh): Add Azure SSO Config

--- install/argocd/install.sh
+++ install/argocd/install.sh
@@ -13,6 +13,8 @@ helm repo add argo https://argoproj.github.io/argo-helm
 
 helm repo update argo
 
+ARGOCD_APP_REGISTRATION_ID="5b59d3e0-04f4-4be4-aff4-b159a8ed4b46"
+
 cat <<EOF > "${ARGOCD_HELM_FILE_SERVICE?}"
 fullnameOverride: "argocd"
 server:
@@ -25,6 +27,20 @@ server:
       nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"
     hosts:
       - argocd.sandbox.wasp.silvios.me
+  config:
+    url: https://argocd.sandbox.wasp.silvios.me
+    oidc.config: |
+      name: AzureAD
+      issuer: https://login.microsoftonline.com/${ARM_TENANT_ID}/v2.0
+      clientID: ${ARGOCD_APP_REGISTRATION_ID}
+      clientSecret: \$oidc.azuread.clientSecret
+      requestedIDTokenClaims:
+        groups:
+          essential: true
+      requestedScopes:
+        - openid
+        - profile
+        - email
 EOF
 
 helm upgrade \
