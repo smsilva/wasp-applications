ede6c3d085daa801dfb56cf36924991902bc199c
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-19 20:22:36 -0300

    feat: Install AGIC: Application Gateway Ingress Controller

--- foundation/aks-application-gateway/run.sh
+++ foundation/aks-application-gateway/run.sh
@@ -6,10 +6,13 @@ export SCRIPTS_DIRECTORY="${PWD}/${DIRECTORY_NAME}/scripts"
 export PATH=${PATH}:${SCRIPTS_DIRECTORY}
 
 . load_parameters
+
 show_parameters
 
 create_aks_cluster_with_application_gateway
 
 install_aad_pod_identity
+
+install_azure_gateway_ingress_controller
 
 finish
