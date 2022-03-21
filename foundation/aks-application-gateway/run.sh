5c1361da6acd4c48b81f0ff6185f665cdc6ff25d
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-21 08:42:27 -0300

    refactor(foundation/aks-application-gateway/): Remove aspnet Application Install

--- foundation/aks-application-gateway/run.sh
+++ foundation/aks-application-gateway/run.sh
@@ -9,12 +9,14 @@ export PATH=${PATH}:${SCRIPTS_DIRECTORY}
 
 show_parameters
 
-# create_aks_cluster_with_application_gateway
+create_aks_cluster_with_application_gateway
 
-# install_aad_pod_identity
+install_aad_pod_identity
 
-# install_azure_gateway_ingress_controller
+install_azure_gateway_ingress_controller
 
 install_httpbin_no_tls
+
+# install_httpbin_tls
 
 finish
